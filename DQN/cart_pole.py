e03a8c50136829d572a9e6691e6499ffd5733127
sh-zh-7 <2431297348@qq.com>
2020-02-04 15:05:36 +0800

    Refactor DQN: CartPole demo

--- DQN/cart_pole.py
+++ DQN/cart_pole.py
@@ -64,98 +64,101 @@ class QNet(nn.Module):
         return x
 
 
-def ChooseAction(Q_net, state):
-    EPS_START = 0.9
-    EPS_END = 0.05
-    EPS_DECAY = 1000
-    global count
-    # 这里很奇怪的一点是, 我们能使用类似宏定义的变量，却不能使用普通的全局变量，得通过global声明以后才能用
-    # 更新Epsilon
-    epsilon = EPS_END + (EPS_START - EPS_END) * math.exp(-1 * count / EPS_DECAY)
-    count += 1
-    # 使用epsilon贪心法选择动作
-    # 注意我们要返回整型
-    if random.random() < epsilon:
-        return random.randint(0, action_dim - 1)
-    else:
-        # 看来每次返回一个action的时候，要对我们的action进行编码
-        return np.argmax(Q_net(state).detach().numpy())
+class DQN:
+    def __init__(self):
+        # Environment part
+        self.state_dim = env.observation_space.shape[0]
+        self.action_dim = env.action_space.n
+        # Q net part
+        self.Q_net = QNet(self.state_dim, self.action_dim)  # Choose actions
+        self.optimizer = optim.SGD(self.Q_net.parameters(), lr=0.2)
+        self.loss_fn = nn.MSELoss()
+        # Other
+        self.count = 0
+        self.replay_buffer = ReplayMemory(10000)
 
-
-def Train(Q_net, optimizer, loss_fn, replay_buffer):
-    mini_batch = replay_buffer.sample(BATCH_SIZE)
-    state_batch = [data[0] for data in mini_batch]
-    action_batch = [data[1] for data in mini_batch]
-    # TODO: 了解你这个action值的具体含义
-    reward_batch = [data[2] for data in mini_batch]
-    next_state = [data[3] for data in mini_batch]
-
-    # Forward
-    # 本质是把后续状态对现在状态的估计当做真值。把对现在状态当做估计值（什么乱七八糟的）
-    y_true_batch = []
-    Q_value_batch = Q_net(next_state)        # 神经网络的输出
-    for i in range(BATCH_SIZE):
-        done = mini_batch[i][4]
-        if done:
-            y_true_batch.append(reward_batch[i])
+    def __SelectAction(self, state):
+        EPS_START = 0.9
+        EPS_END = 0.05
+        EPS_DECAY = 1000
+        # 这里很奇怪的一点是, 我们能使用类似宏定义的变量，却不能使用普通的全局变量，得通过global声明以后才能用
+        # 更新Epsilon
+        epsilon = EPS_END + (EPS_START - EPS_END) * math.exp(-1 * self.count / EPS_DECAY)
+        self.count += 1
+        # 使用epsilon贪心法选择动作
+        # 注意我们要返回整型
+        if random.random() < epsilon:
+            return random.randint(0, self.action_dim - 1)
         else:
-            y_true_batch.append(reward_batch[i] + GAMMA * torch.max(Q_value_batch[i], dim=0)[0])
-    y_true_batch = torch.tensor(y_true_batch, dtype=torch.float, requires_grad=True).unsqueeze(1)
-    # 我们估计的状态
-    action_batch = torch.tensor(action_batch, dtype=torch.long).unsqueeze(1)
-    y_pred_batch = Q_net(state_batch).gather(1, action_batch)
+            # 看来每次返回一个action的时候，要对我们的action进行编码
+            return np.argmax(self.Q_net(state).detach().numpy())
 
-    loss = loss_fn(y_pred_batch, y_true_batch)  # 注意这里是当前选择ACTION的Q值
-    # Backward
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+    def __Train(self):
+        mini_batch = self.replay_buffer.sample(BATCH_SIZE)
+        state_batch = [data[0] for data in mini_batch]
+        action_batch = [data[1] for data in mini_batch]
+        reward_batch = [data[2] for data in mini_batch]
+        next_state = [data[3] for data in mini_batch]
 
+        # Forward
+        # 本质是把后续状态对现在状态的估计当做真值。把对现在状态当做估计值（什么乱七八糟的）
+        # 每次只能更新一个动作
+        y_true_batch = []
+        Q_value_batch = self.Q_net(next_state)  # 神经网络的输出
+        for i in range(BATCH_SIZE):
+            done = mini_batch[i][4]
+            if done:
+                y_true_batch.append(reward_batch[i])
+            else:
+                y_true_batch.append(reward_batch[i] + GAMMA * torch.max(Q_value_batch[i], dim=0)[0])
+        y_true_batch = torch.tensor(y_true_batch, dtype=torch.float, requires_grad=True).unsqueeze(1)
+        # 我们估计的状态
+        action_batch = torch.tensor(action_batch, dtype=torch.long).unsqueeze(1)
+        y_pred_batch = self.Q_net(state_batch).gather(1, action_batch)
 
-count = 0
-if __name__ == "__main__":
-    # 环境部分
-    env = gym.make('CartPole-v0')
-    state_dim = env.observation_space.shape[0]
-    action_dim = env.action_space.n
-    # 多层感知机部分
-    Q_net = QNet(state_dim, action_dim)
-    optimizer = optim.SGD(Q_net.parameters(), lr=0.2)
-    loss = nn.MSELoss()
-    # 其他必要的设定
-    replay_buffer = ReplayMemory(10000)
+        loss = self.loss_fn(y_pred_batch, y_true_batch)  # 注意这里是当前选择ACTION的Q值
+        # Backward
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
-    for episode in range(EPISODE):
+    def Episode(self):
         state = env.reset()     # State就是各个状态的编码(在这里是传感器的返回值)，多个不同的状态堆叠形成向量
         for step in range(STEP):
             # 选择动作并与环境交互, 获得奖励
-            action = ChooseAction(Q_net, state)
+            action = self.__SelectAction(state)
             next_state, reward, done, __ = env.step(action)
             reward = -1 if done else 0.1
             # 更新
-            replay_buffer.push(state, action, reward, next_state, done)
+            self.replay_buffer.push(state, action, reward, next_state, done)
             state = next_state
-
+            if len(self.replay_buffer) > BATCH_SIZE:
+                self.__Train()
             if done:
                 break
-            if len(replay_buffer) > BATCH_SIZE:
-                Train(Q_net, optimizer, loss, replay_buffer)
 
-        # 测试部分
+    def Test(self, episode, TEST=10):
         if episode % 100 == 0:
             total_reward = 0
-            for i in range(10):
+            for i in range(TEST):
                 state = env.reset()
                 for j in range(STEP):
                     env.render()
-                    action = ChooseAction(Q_net, state)
+                    action = self.__SelectAction(state)
                     state, reward, done, _ = env.step(action)
                     total_reward += reward
                     if done:
                         break
-            avg_reward = total_reward / 10
-            print("Episode: ", episode, "Evaluation Average Reward:", avg_reward)
+            avg_reward = total_reward / TEST
+            print("Episode: ", (episode + 100), "Evaluation Average Reward:", avg_reward)
 
+if __name__ == "__main__":
+    # 环境部分
+    env = gym.make('CartPole-v0')
+    solution = DQN()
+    for episode in range(EPISODE):
+        solution.Episode()
+        solution.Test(episode)
     env.close()
 
 
